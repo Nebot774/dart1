26d9493e19045119bf46c4c49c4e4eb46fae6375
adria <adriclemenbot@gmail.com>
2024-01-15 10:03:39 +0100

    Commit:clase comarca y coenxion a api terminada falta implementar el main

--- bin/api_service.dart
+++ bin/api_service.dart
@@ -1,10 +1,49 @@
+//importacion librerias
+import 'dart:convert';
+
 import 'package:args/args.dart';
+import 'package:http/http.dart' as http;
 
 const String version = '0.0.1';
 
-//PARA MANEJAR LAS COLICITUDES A LA API REST
+//funcion para obtener un listado de comarcas de una provincia
+//recibe el nombre de la provincia como argumento
+Future<List<dynamic>> getComarcas(String provincia) async {
+  //Construimos la URL utilizando la provincia proporcionada
+  var url = 'https://node-comarcas-rest-server-production.up.railway.app/api/comarcas/$provincia';
 
+  //Realizamos una solicitud GET a la URL y esperamos la respuesta
+  var respuesta = await http.get(Uri.parse(url)); //await espera a que se complete la operacion para poder seguir
 
+  //verificamos el codigo de estado de la respuesta
+  if (respuesta.statusCode == 200) {
+    //si el codigo de estado es 200 que seria exitoso descodificamos el cuerpo de la respuesta JSON  a una lsita dina
+    return jsonDecode(respuesta.body);
+  } else {
+    //si el codigo de estado no es 200 lanzamos una excepcion
+    throw Exception('Error en la solicitud de la provincia $provincia');
+  }
+}
+
+  //funcion para obtener informacion detallada de una comarca
+//recibe el nombre de la comarca como argumento
+  Future<Map<String, dynamic>> getInfoComarcas(String comarca) async {
+    //Construimos la URL utilizando la provincia proporcionada
+    var url = 'https://node-comarcas-rest-server-production.up.railway.app/api/comarcas/infoComarca/$comarca';
+
+    //Realizamos una solicitud GET a la URL y esperamos la respuesta
+    var respuesta = await http.get(Uri.parse(url)); //await espera a que se complete la operacion para poder seguir
+
+    //verificamos el codigo de estado de la respuesta
+    if (respuesta.statusCode == 200) {
+      //si el codigo de estado es 200 que seria exitoso descodificamos el cuerpo de la respuesta JSON  a una lsita dina
+      return jsonDecode(respuesta.body);
+    } else {
+      //si el codigo de estado no es 200 lanzamos una excepcion
+      throw Exception('Error en la solicitud de la informacion de la comarca $comarca');
+    }
+
+  }
 
 
 
--- bin/comarca.dart
+++ bin/comarca.dart
@@ -7,26 +7,46 @@ const String version = '0.0.1';
 
 class Comarca {
   String nombre;
-  String capital;
-  String poblacion;
-  String img;
-  String desc;
-  double latitud;
-  double longitud;
+  String? capital;
+  String? poblacion;
+  String? img;
+  String? desc;
+  double? latitud;
+  double? longitud;
 
+  //constructor normal
   Comarca({
     required this.nombre,
-    required this.capital,
-    required this.poblacion,
-    required this.img,
-    required this.desc,
-    required this.latitud,
-    required this.longitud,
+    this.capital,
+    this.poblacion,
+    this.img,
+    this.desc,
+    this.latitud,
+    this.longitud,
   });
+
+  //constructor que inicializa una comarca a partir de un objeto JSON
+  Comarca.fromJSON(Map<String, dynamic> json)
+    : nombre=json['nombre'],
+      capital=json['capital'],
+      poblacion=json['poblacion'],
+      img=json['img'],
+      desc=json['desc'],
+      //extaemos la latitud y la longitud de las primeras dos posiciones del vector
+        latitud = json['coordenadas'] != null ? json['coordenadas'][0] : null,
+        longitud = json['coordenadas'] != null ? json['coordenadas'][1] : null;
+
+
 
   @override
   String toString() {
-    return 'Comarca: $nombre, Capital: $capital, Población: $poblacion, Imagen: $img, Descripción: $desc, Latitud: $latitud, Longitud: $longitud';
+    // Crea un String con la información de la comarca formateada.
+    return 'nombre: $nombre\n'
+        'capital: ${capital ?? "No disponible"}\n'
+        'población: ${poblacion ?? "No disponible"}\n'
+        'Imagen: ${img ?? "No disponible"}\n'
+        'descripción: ${desc ?? "No disponible"}\n'
+        'Coordenadas: (${latitud?.toStringAsFixed(7)}, ${longitud?.toStringAsFixed(7)})';
   }
 }
 
