d5274a00c271752465b001164cfa9fd4198ff014
adrcle <adriclemenbot@gmail.com>
2024-01-16 15:37:49 +0100

    Commit:acabado falta probarlo

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,46 @@
 import 'package:args/args.dart';
+//importamos las otras dos clases
+import 'api_service.dart';
+import 'comarca.dart'
 
 const String version = '0.0.1';
 
 //PARA EL PUNTO DE ENTRADA Y LA LOGICA DE MANEJO DE LOS ARGUMENTOS DE LA LINEA DE COMANDOS
+void main(List<String> args) async {
+  try {
+    // Verificamos que se han pasado los argumentos suficientes
+    if (args.length >= 2) {
+      var comando = args[0];
+      var nombre = args[1];
+
+      if (comando == 'comarques') {
+        // Manejamos el comando 'comarques'
+        var comarcas = await getComarcas(nombre);
+        // Imprimimos la lista de comarcas o procesa como necesites
+        print(comarcas);
+      } else if (comando == 'infocomarca') {
+        // Maneja el comando 'infocomarca'
+        var infoComarcaJson = await getInfoComarca(nombre);
+        var comarca = Comarca.fromJSON(infoComarcaJson);
+        print(comarca.toString());
+      } else {
+        print('Comando no reconocido.');
+      }
+    } else {
+      print('Argumentos insuficientes. Uso: dart run ejercicio [comando] [nombre]');
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
